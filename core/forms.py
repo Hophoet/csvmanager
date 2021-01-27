426acb403a4a068e2a45323b8acea9f1e4f9359b
hophoet <hophoet@gmail.com>
2021-01-27 18:10:15 +0000

    (forms) - update csv form model fields

--- core/forms.py
+++ core/forms.py
@@ -5,4 +5,4 @@ from .models import Csv
 class CsvModalForm(forms.ModelForm):
     class Meta:
         model = Csv
-        fields = ('file1', 'file2',)
+        fields = ('file1', 'file2', 'actions')
