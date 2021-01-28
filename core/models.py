ddad271623301657b6f06efb6fa412630ea7036a
hophoet <hophoet@gmail.com>
2021-01-28 01:04:23 +0000

    (models) - add csv model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+#
+
+
+class Csv(models.Model):
+    file1 = models.FileField(upload_to='csvs')
+    file2 = models.FileField(upload_to='csvs')
+    uploaded = models.DateTimeField(auto_now_add=True)
+    activated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f'File id:{self.id}'
